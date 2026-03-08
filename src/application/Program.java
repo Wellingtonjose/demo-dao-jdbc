d1f59f14eb9a8508b89b849c48826268a52bb328
wellington <wellingtoneeee@gmail.com>
2026-03-08 12:27:26 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,5 +14,6 @@ public class Program {
 		Department dp = new Department(1, "Wellington");
 		Seller seller = new Seller(1, "Gaviao", "gaviao@gmail.com", LocalDate.parse("20/02/1996", fm), 2000.00, 1,dp);
 		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerDa();
 	}
 }
